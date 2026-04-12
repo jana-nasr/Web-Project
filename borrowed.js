dc29583ff9d03f04b9b7776fcc96f8e55e2ac388
mariamwaleed <mariamwaleeed13@gmail.com>
2026-04-12 13:59:44 +0200

    my changes

---
+++ borrowed.js
@@ -0,0 +1,105 @@
+const searchinput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+const viewButtons = document.querySelectorAll(".view-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+
+searchinput.addEventListener("input", function () {
+    const value = searchinput.value.toLowerCase();
+
+    cards.forEach(card => {
+        const text = card.textContent.toLowerCase();
+
+        if (text.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
+
+viewButtons.forEach(button => {
+    button.addEventListener("click", function () {
+
+        const card = this.parentElement;
+
+        const title = card.querySelector("h3").innerText;
+        const author = card.querySelectorAll("p")[1].innerText;
+        const category = card.querySelectorAll("p")[2].innerText;
+        const status = card.querySelector(".status").innerText;
+
+        const pages = card.dataset.pages;
+        const lang = card.dataset.language;
+        const desc = card.dataset.desc;
+        const borrow = card.dataset.borrowed;
+
+        document.getElementById("modalTitle").innerText = title;
+        document.getElementById("modalAuthor").innerText = author;
+        document.getElementById("modalCategory").innerText = category;
+        document.getElementById("modalStatus").innerText = "Status: " + status;
+        document.getElementById("modalPages").innerText = "Pages: " + pages;
+        document.getElementById("modalLang").innerText = "Language: " + lang;
+        document.getElementById("modalborrow").innerText = "Borrowed: " + borrow;
+        document.getElementById("modalDesc").innerText = desc;
+
+        modal.style.display = "block";
+    });
+});
+
+closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+const borrowButtons = document.querySelectorAll(".card button:nth-of-type(2)");
+
+borrowButtons.forEach(button => {
+    button.addEventListener("click", function () {
+
+        const card = this.parentElement;
+        const title = card.querySelector("h3").innerText;
+
+        const statusElement = card.querySelector(".status");
+        statusElement.innerText = "Borrowed";
+
+        statusElement.classList.remove("available");
+        statusElement.classList.add("not-available");
+
+        this.disabled = true;
+
+
+    });
+});
+
+function filterBooks(type) {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        const status = card.dataset.status;
+        const category = card.dataset.category;
+
+        if (type === "all") {
+            card.style.display = "block";
+        }
+        else if (type === status || type === category) {
+            card.style.display = "block";
+        }
+        else {
+            card.style.display = "none";
+        }
+    });
+}
+const removeButtons = document.querySelectorAll(".remove-btn");
+
+removeButtons.forEach(btn => {
+    btn.addEventListener("click", function () {
+        const card = this.parentElement;
+
+        const bookName = card.querySelector("h3").innerText;
+
+
+
+
+        card.remove();
+
+    });
+});
---
+++ services_Admin.js
@@ -0,0 +1,118 @@
+const searchinput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+const editBtn = document.getElementById("editToggle");
+const saveBtn = document.getElementById("saveBtn");
+
+searchinput.addEventListener("input", function () {
+    const value = searchinput.value.toLowerCase();
+
+    cards.forEach(card => {
+        const text = card.textContent.toLowerCase();
+
+        if (text.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
+
+const deleteButtons = document.querySelectorAll(".delete-btn");
+
+deleteButtons.forEach(btn => {
+    btn.addEventListener("click", function () {
+        const card = this.parentElement;
+
+        const bookName = card.querySelector("h3").innerText;
+
+
+
+
+        card.remove();
+
+    });
+});
+
+
+
+
+
+
+
+let currentCard = null;
+
+document.querySelectorAll(".edit-btn").forEach(btn => {
+    btn.addEventListener("click", function () {
+
+        currentCard = this.parentElement;
+
+        document.getElementById("modalTitle").value =
+            currentCard.querySelector("h3").innerText;
+
+        document.getElementById("modalAuthor").value =
+            currentCard.querySelectorAll("p")[1].innerText.replace("Author : ", "");
+
+        document.getElementById("modalCategory").value =
+            currentCard.querySelectorAll("p")[2].innerText.replace("category : ", "");
+
+        document.getElementById("modalDesc").value =
+            currentCard.dataset.desc || "";
+
+        document.getElementById("modalPages").value =
+            currentCard.dataset.pages || "pages: ";
+
+        document.getElementById("modalLang").value =
+            currentCard.dataset.language || "language: ";
+
+        document.getElementById("modalStatus").value =
+            currentCard.dataset.status || "statues: ";
+
+        document.getElementById("modalborrow").value =
+            currentCard.dataset.borrowed || "borrowed: ";
+
+        modal.style.display = "block";
+    });
+});
+saveBtn.onclick = () => {
+
+    if (!currentCard) return;
+
+
+    currentCard.querySelector("h3").innerText =
+        document.getElementById("modalTitle").value;
+
+
+    currentCard.querySelectorAll("p")[1].innerText =
+        "Author : " + document.getElementById("modalAuthor").value;
+
+
+    currentCard.querySelectorAll("p")[2].innerText =
+        "category : " + document.getElementById("modalCategory").value;
+
+
+    currentCard.dataset.desc =
+        document.getElementById("modalDesc").value;
+
+    currentCard.dataset.pages =
+        document.getElementById("modalPages").value;
+
+    currentCard.dataset.language =
+        document.getElementById("modalLang").value;
+
+    currentCard.dataset.status =
+        document.getElementById("modalStatus").value;
+
+    currentCard.dataset.borrowed =
+        document.getElementById("modalborrow").value;
+
+    modal.style.display = "none";
+};
+closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+
+
