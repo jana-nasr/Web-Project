f92f3002e98de859deb48ed7d88f7dfd0d531359
11410120240181@stud.cu.edu.eg <11410120240181@stud.cu.edu.eg>
2026-05-12 23:09:59 +0300

    update

--- Library_backend/users/urls.py
+++ Library_backend/users/urls.py
@@ -2,18 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # User Profile API
-    path('api/profile/',               views.api_user_profile,  name='api_user_profile'),
-    path('api/profile/update/',        views.api_update_profile, name='api_update_profile'),
 
-    # Borrow / Return
-    path('api/borrow/<int:book_id>/',  views.api_borrow_book,   name='api_borrow_book'),
-    path('api/return/<int:borrow_id>/',views.api_return_book,   name='api_return_book'),
+    # HTML Pages
+    path('profile/', views.profile_page, name='profile'),
+    path('admin-profile/', views.admin_profile_page, name='admin_profile'),
+    path('login/', views.login_page, name='login'),
+    path('signup/', views.signup_page, name='signup'),
 
-    # Admin APIs
-    path('api/admin/dashboard/',                    views.api_admin_dashboard, name='api_admin_dashboard'),
-    path('api/admin/delete-user/<int:user_id>/',    views.api_delete_user,     name='api_delete_user'),
+    # APIs
+    path('api/profile/', views.api_user_profile, name='api_user_profile'),
+    path('api/profile/update/', views.api_update_profile, name='api_update_profile'),
 
-    # Register (called from signup.js)
-    path('api/register/',              views.api_register,      name='api_register'),
+    path('api/borrow/<int:book_id>/', views.api_borrow_book, name='api_borrow_book'),
+    path('api/return/<int:borrow_id>/', views.api_return_book, name='api_return_book'),
+
+    path('api/admin/dashboard/', views.api_admin_dashboard, name='api_admin_dashboard'),
+    path('api/admin/delete-user/<int:user_id>/', views.api_delete_user, name='api_delete_user'),
+
+    path('api/register/', views.api_register, name='api_register'),
 ]
